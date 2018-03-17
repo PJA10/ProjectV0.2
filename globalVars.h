dd2b7f514a3eaa0d6dd5bbe451b8f35879b25ad1
PJA10 <alon.w.10@gmail.com>
2018-03-17 23:48:30 +0200

    doc

--- globalVars.h
+++ globalVars.h
@@ -4,15 +4,15 @@
 
 #include "dataStructures.h"
 
-extern int IC; 
-extern int DC;
-extern int logIsOn;
-extern labelPtr labelTable;
-extern const commandInfo actionCommandsArray[];
-extern const char *registers[];
-extern int actionMemoryBase[];
-extern int dataMemoryBase[];
-extern const char *addressingModes[];
-extern const char base32[];
+extern int IC; /*the instruction counter*/
+extern int DC; /*the data counter*/
+extern int logIsOn; /*a flag for log print*/
+extern labelPtr labelTable; /*a linked list of all the labels*/
+extern const commandInfo actionCommandsArray[]; /*an array with every action command with info*/
+extern const char *registers[]; /*a array of all the registers names*/
+extern int actionMemoryBase[]; /*the action memory base*/
+extern int dataMemoryBase[]; /*the data memory base*/
+extern const char *addressingModes[]; /*a array of all the addressing modes names - for printing*/
+extern const char base32[]; /*the strange 32 base*/
 
 #endif
